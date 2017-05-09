a6a3457349998babfaf50183794d9a9b10f1837f
Ezex360 <eigiachero@gmail.com>
2017-05-09 19:54:35 -0300

    Creacion de tablas

--- src/main/java/trivia/App.java
+++ src/main/java/trivia/App.java
@@ -1,22 +1,45 @@
 package trivia;
+import java.util.Scanner;
 import org.javalite.activejdbc.Base;
 import trivia.User;
+import trivia.Category;
+import trivia.Question;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
-      Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/trivia", "proyecto", "felipe");
+public class App{
 
+    public static void main( String[] args ){
+    	clearScreen();
+    	cargarUsuario();
+
+    }
+
+    private static void cargarUsuario(){
+    	clearScreen();
+    	System.out.println("Interfaz de Creacion de Usuario");
+    	Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/trivia", "proyecto", "felipe");
+      
       User u = new User();
-      u.set("username", "Maradona");
-      u.set("password", "messi");
+    	System.out.println("Ingrese Nombre de Usuario");
+    	String uname = leerTeclado();
+      System.out.println("Ingrese contraseña");
+      String upass = leerTeclado();    	
+      u.set("username", uname);
+      u.set("password", upass);
       u.saveIt();
 
       Base.close();
     }
+
+    public static String leerTeclado(){
+			String entradaTeclado = "";
+    	Scanner entradaEscaner = new Scanner (System.in); 
+    	entradaTeclado = entradaEscaner.nextLine();
+    	return entradaTeclado;
+		}
+
+		public static void clearScreen() {  
+	    System.out.print("\033[H\033[2J");  
+	    System.out.flush();  
+	  }  
+
 }
